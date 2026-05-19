8ce362693a9044d04d5d6596cc90a0a0803df714
Ayush <ayush2ojha22ayush@gmail.com>
2026-05-19 05:54:07 +0530

    Fix lint warnings and update project structure

--- app/src/main/java/com/mohanic/todo/MainActivity.kt
+++ app/src/main/java/com/mohanic/todo/MainActivity.kt
@@ -5,8 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,9 +17,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TodoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    TodoScreen(modifier = Modifier.padding(innerPadding))
-                }
+                TodoScreen(modifier = Modifier.fillMaxSize())
             }
         }
     }
