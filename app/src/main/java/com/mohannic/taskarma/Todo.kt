dcb396a110860ed36df8461c7b5f8719bf0f91e1
Ayush <ayush2ojha22ayush@gmail.com>
2026-07-04 18:31:03 +0530

    feat: add groupId foreign key to Todo

--- app/src/main/java/com/mohannic/taskarma/Todo.kt
+++ app/src/main/java/com/mohannic/taskarma/Todo.kt
@@ -1,13 +1,27 @@
 package com.mohannic.taskarma
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "todos")
+@Entity(
+    tableName = "todos",
+    foreignKeys = [
+        ForeignKey(
+            entity = TodoGroup::class,
+            parentColumns = ["id"],
+            childColumns = ["groupId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ],
+    indices = [Index(value = ["groupId"])]
+)
 data class Todo(
     @PrimaryKey(autoGenerate = true)
     val id: Long = 0L,
     val text: String,
     val isDone: Boolean = false,
     val isArchived: Boolean = false,
+    val groupId: Long,
 )
